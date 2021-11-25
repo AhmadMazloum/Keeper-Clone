5ad0c0bedde91360a67a63aeb0de52ded27742f6
Ahmad <ahmadmazloum98@gmail.com>
2021-11-25 11:31:05 +0200

    adding notes array to render notes from it

---
+++ src/notes.js
@@ -0,0 +1,21 @@
+const notes = [
+    {
+        id : 1,
+        title : "Note one",
+        content : "This is note number one behold the creation of note."
+
+    },
+    {
+        id : 2,
+        title :"Note two",
+        content  : "No one remembers the second man on the moon."
+
+    },
+    {
+        id : 3,
+        title : "Note three",
+        content : "PS. The second man on the moon was Buzz Aldrin."
+
+    },
+]
+export default notes;
